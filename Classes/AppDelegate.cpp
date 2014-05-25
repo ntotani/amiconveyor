5d9953f3adcc05789360403b27891ceef09f1501
Naoyuki Totani <nyk.totani@gmail.com>
2014-05-25 20:02:02 +0900

    BGM停止

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,6 +1,7 @@
 #include "AppDelegate.h"
 #include "Common/LBFileUtils.h"
 #include "Scene/Game/GameScene.h"
+#include "SimpleAudioEngine.h"
 
 USING_NS_CC;
 
@@ -51,7 +52,7 @@ void AppDelegate::applicationDidEnterBackground() {
     Director::getInstance()->stopAnimation();
 
     // if you use SimpleAudioEngine, it must be pause
-    // SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
+    CocosDenshion::SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
 }
 
 // this function will be called when the app is active again
@@ -59,7 +60,7 @@ void AppDelegate::applicationWillEnterForeground() {
     Director::getInstance()->startAnimation();
 
     // if you use SimpleAudioEngine, it must resume here
-    // SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
+    CocosDenshion::SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
 }
 
 static CustomCommand saveToFileCommand;
